bb6439762b09e060df5f57a5518bee9762fca24e
codedario <darionunez1999@gmail.com>
2019-11-13 10:35:58 +0000

    Add express server and initialisation files

---
+++ mongodb-service/src/start.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+import { MongoConnector } from '../../common-service/src/mongodbConnector';
+import { ApiServer } from './api-server';
+
+export async function start(): Promise<ApiServer> {
+    const mongoConnector = new MongoConnector();
+    const apiServer = new ApiServer();
+    await apiServer.start();
+    await mongoConnector.connect();
+    const graceful = async () => {
+        await mongoConnector.disconnect();
+        await apiServer.stop();
+        process.exit(0);
+    };
+
+    // Stop graceful
+    process.on('SIGTERM', graceful);
+    process.on('SIGINT', graceful);
+
+    return apiServer;
+}
