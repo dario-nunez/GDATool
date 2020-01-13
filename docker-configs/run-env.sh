ba192a471f0a7dde366b42c4c6f59dfea5462cff
codedario <darionunez1999@gmail.com>
2020-01-13 20:53:00 +0000

    Delete AWS keys and fix the docker environment running script

--- docker-configs/run-env.sh
+++ docker-configs/run-env.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.yaml
+docker-compose -f docker-compose.yaml up
 
 # Use the command below if Docker complains about missing images. 
 # WARNING: It will clear image data.
